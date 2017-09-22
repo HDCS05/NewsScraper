43813fbb1c7dc1d5cec40bf7ee96359881b2fd9d
Hilton <hdcs05@gmail.com>
2017-09-22 03:43:57 -0400

    still to do notes and validation of saved

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var cheerio = require("cheerio");
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
+var result = [];
 
 // Initialize Express
 var app = express();
@@ -30,7 +31,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect("mongodb://localhost/hwScrapedData");
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -51,47 +52,49 @@ app.set("view engine", "handlebars");
 // Routes
 // ======
 
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+
 // A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
+ 
   request("http://www.npr.org/sections/politics-pop-culture/", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
+    // Save an empty result object
+    //result = [];
     $(".item-info").each(function(i, element) {
 
-      // Save an empty result object
-      var result = {};
+      var vtitle = $(this).children("h2").children("a").text();
+      var vlink = $(this).children("h2").children("a").attr("href");
+      var vdescription = $(this).children("p").children("a").text();
+      var vid = i;
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("h2 > a").text();
-      result.link = $(this).children("h2 > a").attr("href");
-      result.description = $(this).children("p > a").text();
-
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
-
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
+      result.push({
+        id: vid,
+        title: vtitle,
+        link: vlink,
+        description: vdescription
       });
 
     });
+    var hbsObject = {
+      scrapednews: result
+    };
+    console.log(result);
+    res.render("scraped", hbsObject);
   });
   // Tell the browser that we finished scraping the text
-  res.send("Scrape Complete");
+  //res.render("scraped", result);
+  //res.send("Scrape Complete");
 });
 
 // This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
+app.get("/savedArticles", function(req, res) {
   // Grab every doc in the Articles array
   Article.find({}, function(error, doc) {
     // Log any errors
@@ -100,60 +103,51 @@ app.get("/articles", function(req, res) {
     }
     // Or send the doc to the browser as a json object
     else {
-      res.json(doc);
+      var hbsObject = {
+        oSavedNews: doc
+      };
+      //res.json(doc);
+      res.render("savednews",hbsObject);
     }
   });
 });
 
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  Article.findOne({ "_id": req.params.id })
-  // ..and populate all of the notes associated with it
-  .populate("note")
-  // now, execute our query
-  .exec(function(error, doc) {
+// When save article button is clicked
+app.post("/:id", function(req, res) {
+
+  //create empty array
+  var oData = {};
+  var vindex = req.params.id
+
+  oData.title = result[vindex].title;
+  oData.link = result[vindex].link;
+  oData.description = result[vindex].description;
+
+  //console.log("-----------------" + req)
+  // console.log(oData.title);
+  // console.log(oData.link);
+  // console.log(oData.description);
+  // console.log("-----------------" + oData);
+
+  // Using our Article model, create a new entry
+  // This effectively passes the result object to the entry (and the title and link)
+  var entry = new Article(oData);
+
+  // Now, save that entry to the db
+  entry.save(function(err, doc) {
     // Log any errors
-    if (error) {
-      console.log(error);
+    if (err) {
+      console.log(err);
     }
-    // Otherwise, send the doc to the browser as a json object
+    // Or log the doc
     else {
-      res.json(doc);
+      console.log(doc);
+      res.redirect("/savedArticles");
     }
   });
+
 });
 
-
-// Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  var newNote = new Note(req.body);
-
-  // And save the new note the db
-  newNote.save(function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise
-    else {
-      // Use the article id to find and update it's note
-      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-      // Execute the above query
-      .exec(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        else {
-          // Or send the document to the browser
-          res.send(doc);
-        }
-      });
-    }
-  });
-});
 
 
 // Listen on port 3000
