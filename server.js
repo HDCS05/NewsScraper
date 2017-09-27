b5ec5270175537e430cec8626e68dba9a0b3b15b
Hilton <hdcs05@gmail.com>
2017-09-27 14:51:34 -0400

    changed capital letters

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
