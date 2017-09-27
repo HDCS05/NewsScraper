00dbb3d4eabb623a92201da23b7e5fe0530a2bb4
Hilton <hdcs05@gmail.com>
2017-09-27 14:55:59 -0400

    changed port

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+var port = process.env.PORT || 3000;
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
@@ -175,6 +176,6 @@ app.post("/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("App running on port 3000!");
 });
