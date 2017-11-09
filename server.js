e96030c6071944a794936eac0e5a66e9f6784afb
Hilton <hdcs05@gmail.com>
2017-11-09 17:12:36 -0500

    chaged reference Article.js to article.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 // Requiring our Note and Article models
 var Note = require("./models/note.js");
-var Article = require("./models/Article.js");
+var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
