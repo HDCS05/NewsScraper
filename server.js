e7a95012daf63220cb028960b42a4f625804540a
Hilton <hdcs05@gmail.com>
2017-11-09 17:09:14 -0500

    removed button again and chaged reference Note.js to note.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
+var Note = require("./models/note.js");
 var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
