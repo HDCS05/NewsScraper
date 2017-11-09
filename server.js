8740cafeb280c47ef3e5dc8c80a1474663ab2163
Hilton <hdcs05@gmail.com>
2017-11-09 16:44:12 -0500

    removed save note button until implemented

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 // Requiring our Note and Article models
-var Note = require("./models/note.js");
-var Article = require("./models/article.js");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
@@ -175,7 +175,7 @@ app.post("/:id", function(req, res) {
 
 
 
-// Listen on port 3000
+// Listen on port defined at the top
 app.listen(port, function() {
   console.log("App running on port 3000!");
 });
